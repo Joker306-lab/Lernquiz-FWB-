78f5266bd2c2f0cfa68f0ab7a38615f96cbc2dc6
Joker306-lab <joker.zna.muc@gmail.com>
2026-03-20 21:43:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-function startQuiz(){
-window.location.href="quiz.html";
+function openModules(){
+const el=document.getElementById("modules");
+el.classList.toggle("hidden");
 }
-
-document.getElementById("startButton").addEventListener("click",startQuiz);
-
-const avatarStage=document.getElementById("avatarStage");
-
-function move(x,y){
-const dx=(window.innerWidth/2-x)/70;
-const dy=(window.innerHeight/2-y)/90;
-avatarStage.style.transform=`translate(${dx}px,${dy}px)`;
-}
-
-document.addEventListener("mousemove",e=>move(e.clientX,e.clientY));
-document.addEventListener("touchmove",e=>{
-const t=e.touches[0];
-if(t)move(t.clientX,t.clientY);
-},{passive:true});
-
-document.addEventListener("touchend",()=>{
-avatarStage.style.transform="translate(0,0)";
-});
