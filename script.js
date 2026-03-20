3ce4052ee5596f2f6d3dee6ae9b289c594793336
Joker306-lab <joker.zna.muc@gmail.com>
2026-03-20 21:26:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-function startQuiz() {
-  window.location.href = "quiz.html";
+function startQuiz(){
+window.location.href="quiz.html";
 }
 
-const startButton = document.getElementById("startButton");
-if (startButton) {
-  startButton.addEventListener("click", startQuiz);
+document.getElementById("startButton").addEventListener("click",startQuiz);
+
+const avatarStage=document.getElementById("avatarStage");
+
+function move(x,y){
+const dx=(window.innerWidth/2-x)/70;
+const dy=(window.innerHeight/2-y)/90;
+avatarStage.style.transform=`translate(${dx}px,${dy}px)`;
 }
 
-const avatarStage = document.getElementById("avatarStage");
+document.addEventListener("mousemove",e=>move(e.clientX,e.clientY));
+document.addEventListener("touchmove",e=>{
+const t=e.touches[0];
+if(t)move(t.clientX,t.clientY);
+},{passive:true});
 
-function moveAvatar(clientX, clientY) {
-  if (!avatarStage) return;
-  const offsetX = (window.innerWidth / 2 - clientX) / 65;
-  const offsetY = (window.innerHeight / 2 - clientY) / 85;
-  avatarStage.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-}
-
-document.addEventListener("mousemove", (event) => {
-  moveAvatar(event.clientX, event.clientY);
-}, { passive: true });
-
-document.addEventListener("touchmove", (event) => {
-  const touch = event.touches && event.touches[0];
-  if (!touch) return;
-  moveAvatar(touch.clientX, touch.clientY);
-}, { passive: true });
-
-document.addEventListener("touchend", () => {
-  if (!avatarStage) return;
-  avatarStage.style.transform = "translate(0px, 0px)";
-}, { passive: true });
+document.addEventListener("touchend",()=>{
+avatarStage.style.transform="translate(0,0)";
+});
