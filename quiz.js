91701a2195a471cad66c00e91ed67e3613583193
Joker306-lab <joker.zna.muc@gmail.com>
2026-03-21 07:33:24 +0100

    Delete quiz.js

--- quiz.js
+++
@@ -1,5 +0,0 @@
-const toggleButton=document.getElementById('toggleMaterials');
-const materialsSection=document.getElementById('materialsSection');
-if(toggleButton&&materialsSection){
-  toggleButton.addEventListener('click',()=>materialsSection.classList.toggle('hidden'));
-}
