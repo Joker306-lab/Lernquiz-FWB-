518a23c5d99c8da752360cc139de2964fee425e3
Joker306-lab <joker.zna.muc@gmail.com>
2026-03-21 07:34:12 +0100

    Delete start.js

--- start.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("startBtn").addEventListener("click",async()=>{
-const audio=new Audio("assets/start.mp3");
-try{await audio.play()}catch(e){}
-setTimeout(()=>{window.location="quiz.html"},500)
-})
